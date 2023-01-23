b1e874c211a8ac29dfedde62c1ee1cd2b8596668
samusanc <samusanc@student.42madrid.com>
2023-01-23 13:15:13 +0100

    subiendo prueba

--- ft_memmove.c
+++ ft_memmove.c
@@ -9,8 +9,10 @@
 /*   Updated: 2023/01/17 13:19:25 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include<stdio.h>
 #include<unistd.h>
+
+unsigned long	ft_strlen(char *str);
 
 void	*ft_memmove(void *str1, const void *str2, size_t n)
 {
@@ -18,19 +20,38 @@ void	*ft_memmove(void *str1, const void *str2, size_t n)
 	char	*str1c;
 	char	*str2c;
 
-	i = 0;
+	i = n;
 	str1c = (char*)str1;
 	str2c = (char*)str2;
-	if (n <= 0)
-		return (str1);
-	n = n -1;
-//en vez de hacer de 0 a lo que sea haz de lo que sea a cero restando
-	while (i <= n)
+	while((int)(--i) >= 0)
 	{
-		(str1c[i] = str2c[i]);
-		++i;
+		str1c[i] = str2c[i];
+		printf("%zu", i);
 	}
-	if (str1c[i] == '\0')
-		str1c[i] = '\0';
+	if(n > ft_strlen(str1c))
+		str1c[n] = 0;		
 	return (str1);
 }
+
+// void	*ft_memmove(void *str1, const void *str2, size_t n)
+// {
+// 	size_t	i;
+// 	char	*str1c;
+// 	char	*str2c;
+
+// 	i = 0;
+// 	str1c = (char*)str1;
+// 	str2c = (char*)str2;
+// 	if (n <= 0)
+// 		return (str1);
+// 	n = n -1;
+// //en vez de hacer de 0 a lo que sea haz de lo que sea a cero restando
+// 	while (i <= n)
+// 	{
+// 		(str1c[i] = str2c[i]);
+// 		++i;
+// 	}
+// 	if (str1c[i] == '\0')
+// 		str1c[i] = '\0';
+// 	return (str1);
+// }
