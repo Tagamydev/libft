360faa320edb9b849ad82aa52a171ea10fb54873
samusanc <samusanc@student.42madrid.com>
2023-01-23 15:10:29 +0100

    subiendo prueba

--- ft_memset.c
+++ ft_memset.c
@@ -16,10 +16,12 @@ void	*ft_memset(void *b, int c, size_t len)
 	size_t	i;
 
 	i = 0;
+	if (len == 0)
+		return (b);
 	len = len - 1;
 	while (i <= len)
 	{
-		((unsigned char *)b)[i] = c;
+		*(char*)(b + i) = c;
 		++i;
 	}
 	return (b);
