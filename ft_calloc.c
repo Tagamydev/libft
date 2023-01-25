064d865d4208ac8f50eb8557b97984bed564dc94
samusanc <samusanc@student.42madrid.com>
2023-01-25 16:40:55 +0100

    subiendo prueba

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 16:17:17 by samusanc          #+#    #+#             */
+/*   Updated: 2023/01/25 16:17:20 by samusanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include<stdlib.h>
 
 void	*ft_bzero(void *s, size_t n);
@@ -5,11 +17,12 @@ void	*ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*str;
-	int i;
+	int		i;
 
 	i = -1;
-	if(!(str = (void *)malloc(count * size)))
+	str = (void *)malloc(count * size);
+	if (!str)
 		return (0);
-	ft_bzero(str, count);
+	ft_bzero(str, count * size);
 	return (str);
 }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -9,16 +9,33 @@
 /*   Updated: 2023/01/17 16:25:44 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-// #include<unistd.h>
+#include<unistd.h>
 
-// unsigned long	ft_strlen(char *str);
+unsigned long	ft_strlen(char *str);
 
-// size_t	strlcat(char *dst, const char *src, size_t dstsize)
-// {
-// 	int	dstl;
-	
-// 	if (dstsize <= dstl)
-// 		return (dstl + src);
-// 	dstl = (int)(ft_strlen((char *)(dst)));
-// 	return (dstl + src);
-// }
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	int	dstl;
+	int	i;
+	int	i2;
+
+	dstl = (int)ft_strlen((char *)dst);
+	if(dstsize == 0)
+		return ((int)ft_strlen((char *)src));
+	if ((int)dstsize <= dstl)
+		return (dstl + (int)ft_strlen((char *)src));
+	else
+	{
+		i = dstl;
+		i2 = 0;
+		while (i < (int)(dstsize - 1))
+		{
+			*(char *)(dst + i) = *(char *)(src + i2);
+			++i2;
+			++i;
+		}
+		*(char *)(dst + i) = '\0';
+		return (dstl + (int)ft_strlen((char *)src));
+	}
+	return (dstl + (int)ft_strlen((char *)src));
+}
