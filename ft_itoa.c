6650a7bf53ece52908ad914a9692b8642d93b86f
TagamyVM <samu2004ssmm@gmail.com>
2023-02-04 13:41:26 +0100

    subiendo el itoa sin tester

--- ft_itoa.c
+++ ft_itoa.c
@@ -4,6 +4,30 @@
 
 char	*ft_itoa(int n)
 {
-	ft_yoque(n, 3);
-	return (0);
+	char *str;
+	int		i;
+
+	i = 0;
+	str = (malloc((ft_intlen(n) + 1) * sizeof(char)));
+	if (!str)
+		return (0);
+	if (n == 0)
+	{
+		str[i] = '0';
+		++i;
+		str[i] = '\0';
+		return (str);
+	}
+	if (n < 0)
+		str[i] = '-';
+	else
+		str[i] = '+';
+	++i;
+	while (i != (ft_intlen(n) + 1))
+	{
+		str[i] = ft_getndigit(n, i) + '0';
+		++i;
+	}
+	str[i] = '\0';
+	return (str);
 }
