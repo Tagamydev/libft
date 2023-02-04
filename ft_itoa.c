fc8c2850e8e33bb8092084161ffbebd40ea1fed9
TagamyVM <samu2004ssmm@gmail.com>
2023-02-04 14:48:23 +0100

    subiendo el itoa bueno

--- ft_itoa.c
+++ ft_itoa.c
@@ -8,30 +8,31 @@ char	*ft_itoa(int n)
 	int		i;
 
 	i = 0;
-	str = (malloc((ft_intlen(n) + 1) * sizeof(char)));
+	str = (malloc((ft_intlen(n)) * sizeof(char)));
 	if (!str)
 		return (0);
-	if (n == 0)
-	{
-		str[i] = '0';
-		++i;
-		str[i] = '\0';
-		return (str);
-	}
 	if (n < 0)
 		str[i] = '-';
 	else
 		str[i] = '+';
 	++i;
-	while (i != (ft_intlen(n) + 1))
+	if (n < 0)
 	{
-		str[i] = ft_getndigit(n, i) + '0';
-		++i;
+		while (i != (ft_intlen(n) + 1))
+		{
+			str[i] = ft_getndigit(n, i) + '0';
+			++i;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i != (ft_intlen(n)))
+		{
+			str[i] = ft_getndigit(n, i + 1) + '0';
+			++i;
+		}
 	}
 	str[i] = '\0';
-	if (n < 0)
-		return (str);
-	else
-		++str;
 	return (str);
 }
