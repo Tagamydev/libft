537ec6253d704523bc772ee28e0b4917128d3afe
samusanc <samusanc@student.42madrid.com>
2023-02-06 14:44:20 +0100

    subiendo prueba

--- libft.h
+++ libft.h
@@ -43,6 +43,7 @@ void			*ft_memchr(const void *s, int c, size_t n);
 void			ft_putnbr_fd(int n, int fd);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
 size_t			ft_strlcpy(char *d, const char *s, size_t z);
 size_t			ft_strlcat(char *d, const char *s, size_t z);
 size_t			ft_strlen(const char *s);
