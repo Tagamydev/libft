6978d20e5065ca4e3e02ef83542e2d19a2c7806e
samusanc <samusanc@student.42madrid.com>
2023-02-12 12:14:48 +0100

    subiendo prueba

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,31 +6,13 @@
 /*   By: samusanc <samusanc@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 14:55:06 by samusanc          #+#    #+#             */
-/*   Updated: 2023/02/12 12:09:18 by samusanc         ###   ########.fr       */
+/*   Updated: 2023/02/12 12:14:34 by samusanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft.h"
-
-static char	*ft_putins(char *s1c, char *str, int i, char *s2c)
-{
-	while (s1c[0] != '\0')
-	{
-		str[i] = s1c[0];
-		++i;
-		++s1c;
-	}
-	while (s2c[0] != '\0')
-	{
-		str[i] = s2c[0];
-		++i;
-		++s1c;
-	}
-	str[i] = '\0';
-	return (str);
-}
 
 /*Reserva (con malloc(3)) y devuelve una nueva
 string, formada por la concatenación de ’s1’ y
@@ -50,6 +32,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!str)
 		return (0);
 	i = 0;
-	str = ft_putins(s1c, str, i, s2c);
+	while (s1c[0] != '\0')
+	{
+		str[i] = s1c[0];
+		++i;
+		++s1c;
+	}
+	while (s2c[0] != '\0')
+	{
+		str[i] = s2c[0];
+		++i;
+		++s2c;
+	}
+	str[i] = '\0';
 	return (str);
 }
